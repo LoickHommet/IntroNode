18c8000d6e12c9d225505fefff1c8b7121ad1870
Loick Hommet <hommet.loick@gmail.com>
2022-04-29 16:47:29 +0200

    ok: ajoue Milldeware

--- exo6_middleware/index.js
+++ exo6_middleware/index.js
@@ -2,24 +2,30 @@ const { Socket } = require("dgram");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  let start = performance.now()
   try {
     
     if (req.url == "/") {  
-      timer.start('myTimer1');  
+    
       res.writeHead(200, { "content-type": "text/html" });
       res.write("<h1>Home page</h2>");
-      res.end();
+     
       // console.log(timer.stop('myTimer1'));
     } else {
       res.writeHead(404, { "content-type": "text/html" });
       res.write("<h1> 404 Not Found</h1>");
-      res.end();
+      
     }
   } catch (err) {
     res.writeHead(500, { "content-type": "text/html" });
     res.write("<h1> 500 Internal Server Error</h1>");
-    res.end();
+    
   }
+  // ICI MIDDLEWARE A EXECUTER AVANT D'ENVOYE LA REPONSE
+  res.end();
+  // ICI MIDDLEWARE A EXECUTER APRES D'ENVOYE LA REPONSE
+  let end = performance.now();
+  console.log("requete temp :", end - start)
 });
 
 

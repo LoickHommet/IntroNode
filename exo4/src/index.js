0a076cc95659c6e1f0979e69f405956a91aa0d8d
Loick Hommet <hommet.loick@gmail.com>
2022-04-29 09:08:47 +0200

    ajoue du direname

--- exo4/src/index.js
+++ exo4/src/index.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 
-const text = fs.readFileSync('assets/index.html', {encoding: "utf-8"})
-const ecrire = path.join(text);
-const textLog = fs.readFileSync('src/log.txt', {encoding: "utf-8"})
-const ecrireLog = path.join(textLog);
+// const text = fs.readFileSync('/assets/index.html', {encoding: "utf-8"})
+// const ecrire = path.join(__dirname,text);
+//  const textLog = fs.readFileSync(__dirname, '/src/log.txt', {encoding: "utf-8"})
+const ecrireLog = path.join(__dirname, '/src/log.txt');
 // console.log(textLog)
-console.log(ecrire)
+// console.log(ecrire)
 console.log(ecrireLog)
